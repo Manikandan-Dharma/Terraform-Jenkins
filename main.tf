f477ed943f245d4fce5e7cd782afd80f03396ae0
Manikandan-Dharma <157537135+Manikandan-Dharma@users.noreply.github.com>
2024-02-18 15:35:45 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 provider "aws" {
-  region = "ap-south-1" 
+  region = "us-east-1" 
 }
 
 resource "aws_instance" "SERVEREC2" {
-  ami           = "ami-0449c34f967dbf18a"   # ap-south-1 Asia pacific Mumbai
-  instance_type = "t2.micro"       
+  ami           = "ami-0e731c8a588258d0d"   
+  instance_type = "t2.micro"      
+
   tags = {
     Name = "SERVER-EC2"
   }
