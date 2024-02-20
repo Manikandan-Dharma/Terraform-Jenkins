723433181859addf5a6e6ab5bb3fe0b2126746e6
Manikandan-Dharma <157537135+Manikandan-Dharma@users.noreply.github.com>
2024-02-20 10:39:45 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,13 @@ provider "aws" {
   region = "us-east-1" 
 }
 
-resource "aws_instance" "SERVEREC2" {
+resource "aws_instance" "TOMCAT server" {
   ami           = "ami-0e731c8a588258d0d"   
-  instance_type = "t2.micro"      
+  instance_type = "t2.micro" 
+  key_name = "FINAL KEYPAIR"
+  monitoring = true
 
   tags = {
-    Name = "MySERVER-EC2"
+    Name = "TOMCAT server"
   }
 }
